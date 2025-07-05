85abfd169bd6d003542366e5cd8aadccca36cf20
sipankaj <pankajsingh.nitb@gmail.com>
2025-07-05 14:43:40 -0400

    Initial Commit

--- src/main/java/com/sipankaj/billingaccount/AccountManagementMsApplication.java
+++ src/main/java/com/sipankaj/billingaccount/AccountManagementMsApplication.java
@@ -1,8 +1,7 @@
 package com.sipankaj.billingaccount;
 
-import com.sipankaj.remoteconfig.annotation.EnableRemoteConfig;
+import io.github.sipankaj.remoteconfig.annotation.EnableRemoteConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -12,5 +11,4 @@ public class AccountManagementMsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AccountManagementMsApplication.class, args);
 	}
-
 }
